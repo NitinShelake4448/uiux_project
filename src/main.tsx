9b22c9983b7aa01ad96e41dd89b92283314753b3
Nitin Shelake <ndshelake3@gmail.com>
2026-04-16 20:54:33 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+import App from "./app/App.tsx";
+import ProjectDetail from "./app/ProjectDetail";
+import "./styles/index.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+createRoot(document.getElementById("root")!).render(
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/project/:id" element={<ProjectDetail />} />
+    </Routes>
+  </BrowserRouter>
+);
